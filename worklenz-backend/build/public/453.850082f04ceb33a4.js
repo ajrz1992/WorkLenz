9195e68b0b5753cb5c524584d5f49ce696c76778
Aldo Josue Rodriguez Zuniga <ajrz_1992@hotmail.es>
2025-04-15 19:50:58 -0600

    Reversion de Boton Year y Month

--- worklenz-backend/build/public/453.850082f04ceb33a4.js
+++ worklenz-backend/build/public/453.850082f04ceb33a4.js
@@ -2629,8 +2629,8 @@
                   "nzSize",
                   "ngModelChange",
                 ],
-                ["nz-radio-button", "", "nzValue", "Mes"],
-                ["nz-radio-button", "", "nzValue", "Año"],
+                ["nz-radio-button", "", "nzValue", "month"],
+                ["nz-radio-button", "", "nzValue", "year"],
                 [3, "nzLabel", "nzValue"],
                 [
                   1,
