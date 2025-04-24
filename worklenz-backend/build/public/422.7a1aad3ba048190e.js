2fe95aba3e20e321bedc8cd7c320b0c87f720ffe
Aldo Josue Rodriguez Zuniga <ajrz_1992@hotmail.es>
2025-04-23 23:12:21 -0600

    Seleccionar mes y semana en dropdownlist de calendario

--- worklenz-backend/build/public/422.7a1aad3ba048190e.js
+++ worklenz-backend/build/public/422.7a1aad3ba048190e.js
@@ -14633,7 +14633,7 @@
                   e.Q6J("ngModel", o.task.phase_id)("nzLoading", o.loading)(
                     "nzDropdownMatchSelectWidth",
                     !1
-                  )("nzSize", "small")("nzPlaceHolder", "Seleccionar"),
+                  )("nzSize", "small")("nzPlaceHolder", "Select"),
                   e.xp6(1),
                   e.Q6J("ngForOf", o.phases)("ngForTrackBy", o.trackById));
             },
